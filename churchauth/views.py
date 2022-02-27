25d77b3a5a0f4ca749f86e0683ef5d52b9989628
cynthia-nyambura <nyambucynthia98@gmail.com>
2022-02-27 17:43:05 +0300

    add home and register page

--- churchauth/views.py
+++ churchauth/views.py
@@ -1,7 +1,21 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
-  return HttpResponse("Hello World")
+  return render(request, "authentication/index.html")
+
+def register(request):
+  if request.method== "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Hello {username}, You Have Been Succesfully Registered')
+      return redirect ('home')
+  else:
+    form = UserCreationForm()
+  return render(request, "authentication/register.html",{'form':form})
